009078b60d4665015ec3124c5eb7043bf3906ef4
Michael Maysonet <michaelmaysonet74@gmail.com>
2021-08-26 09:55:02 -0400

    Add more schema linter rules

--- graphql-schema-linter.config.js
+++ graphql-schema-linter.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   schemaPaths: ["./src/**/*.graphql"],
   rules: [
+    "deprecations-have-a-reason",
     "enum-values-sorted-alphabetically",
     "fields-are-camel-cased",
+    "fields-have-descriptions",
     "input-object-values-are-camel-cased",
     "types-are-capitalized",
   ],
