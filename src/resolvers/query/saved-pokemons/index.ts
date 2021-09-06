a62ce1f36c97257ae7989ea66453a7fc0fcf0470
Michael Maysonet <michaelmaysonet74@gmail.com>
2021-09-06 10:11:22 -0400

    DEX-11 - Remove Deprecated Query.savedPokemons

--- src/resolvers/query/saved-pokemons/index.ts
+++
@@ -1,8 +0,0 @@
-import { ResolverContext } from "../../../context/types";
-import { SchemaSavedPokemon } from "../../../schema-types";
-
-export const getSavedPokemons = (
-  _: null,
-  args: null,
-  { dataSources: { store } }: ResolverContext
-): SchemaSavedPokemon[] => store.getPokemonRecords().map(({ id }) => ({ id }));
