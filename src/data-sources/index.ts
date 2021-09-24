c2dc3ec385189f569ac2d56232d0a37f56e1eb81
Michael Maysonet <michaelmaysonet74@gmail.com>
2021-09-24 10:22:21 -0400

    DEX-6 - Remove Store datasource

--- src/data-sources/index.ts
+++ src/data-sources/index.ts
@@ -1,13 +1,10 @@
 import { DataSources } from "apollo-server-core/dist/graphqlOptions";
 import { PokeAPI } from "./poke-api";
-import store, { Store } from "./store";
 
 export interface PokemonDataSources {
   pokeAPI: PokeAPI;
-  store: Store;
 }
 
 export const dataSources = (): DataSources<PokemonDataSources> => ({
   pokeAPI: new PokeAPI(),
-  store,
 });
