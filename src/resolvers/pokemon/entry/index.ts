a4755445bf67768a2bda12f6d74c7456189fda73
Michael Maysonet <michaelmaysonet74@gmail.com>
2021-09-25 16:10:39 -0400

    DEX-0 - Fix Pokemon.entry resolution

--- src/resolvers/pokemon/entry/index.ts
+++ src/resolvers/pokemon/entry/index.ts
@@ -1,30 +1,36 @@
 import { MetaPokemon } from "..";
 import {
+  Maybe,
   SchemaGameVersion,
   SchemaLanguage,
   SchemaPokemonEntryArgs,
 } from "../../../schema-types";
 import { ResolverContext } from "../../../context/types";
+import { FlavorTextEntry } from "../../../data-sources/poke-api/models/pokemon-species";
 
-const DEFAULT_LANGUAGE = SchemaLanguage.en;
-const DEFAULT_VERSION = SchemaGameVersion.blue;
+const filterByLanguage =
+  (filter?: Maybe<SchemaLanguage>): ((e: FlavorTextEntry) => boolean) =>
+  ({ language }) =>
+    language.name === (filter ?? SchemaLanguage.en).toString();
+
+const filterByVersion = (
+  filter?: Maybe<SchemaGameVersion>
+): ((e: FlavorTextEntry) => boolean) =>
+  filter ? ({ version }) => version.name === filter.toString() : () => true;
 
 export const getEntry = async (
   { id }: MetaPokemon,
   { language: maybeLanguage, version: maybeVersion }: SchemaPokemonEntryArgs,
   { dataSources: { pokeAPI } }: ResolverContext
 ): Promise<string | null> => {
-  const _language = (maybeLanguage ?? DEFAULT_LANGUAGE).toString();
-  const _version = (maybeVersion ?? DEFAULT_VERSION).toString();
-
   const { flavor_text_entries } = await pokeAPI.getPokemonSpeciesById(id);
 
-  return (
-    flavor_text_entries
-      .filter(
-        ({ language, version }) =>
-          language.name === _language && version.name === _version
-      )
-      .map(({ flavor_text }) => flavor_text)?.[0] ?? null
-  );
+  const byLanguage = filterByLanguage(maybeLanguage);
+  const byVersion = filterByVersion(maybeVersion);
+
+  const entry = flavor_text_entries
+    .filter((_) => byLanguage(_) && byVersion(_))
+    .map(({ flavor_text }) => flavor_text)?.[0];
+
+  return entry?.split("\n").join(" ").split("\f").join(" ") ?? null;
 };
