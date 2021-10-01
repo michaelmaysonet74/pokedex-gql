99fafcbe91b1591ebb2aa291995646bdadbd94ff
Michael Maysonet <michaelmaysonet74@gmail.com>
2021-10-01 18:42:46 -0400

    DEX-9 - Format Pokemon.abilities as Title

---
+++ src/resolvers/pokemon/abilities/abilities.test.ts
@@ -0,0 +1,34 @@
+import { getAbilities } from ".";
+import { MetaPokemon } from "..";
+import { PokemonAbility } from "../../../data-sources/poke-api/models/pokemon";
+import { fakePokemon } from "../../../fixtures/data-sources/pokemon";
+
+const baseParent: MetaPokemon = {
+  id: "7",
+  _meta: {
+    pokemonDetails: fakePokemon,
+  },
+};
+
+describe("Pokemon.abilities", () => {
+  it("should return abilities as an array of strings", () => {
+    const expectedResult = ["Torrent"];
+    const result = getAbilities(baseParent);
+    expect(result).toEqual(expectedResult);
+  });
+
+  it("should return empty array if _meta.pokemonDetails.abilities is undefined", () => {
+    const updatedParent = {
+      ...baseParent,
+      _meta: {
+        ...baseParent._meta,
+        pokemonDetails: {
+          ...fakePokemon,
+          abilities: undefined as unknown as PokemonAbility[],
+        },
+      },
+    };
+    const result = getAbilities(updatedParent);
+    expect(result).toEqual([]);
+  });
+});
