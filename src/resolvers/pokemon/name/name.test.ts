aae0b1491799cdd79c75251dd31eb21188f2ec4b
michaelmaysonet74 <michaelmaysonet74@gmail.com>
2021-10-02 10:26:03 -0400

    DEX-4 - Add unit test to all Resolvers

---
+++ src/resolvers/pokemon/name/name.test.ts
@@ -0,0 +1,33 @@
+import { getName } from ".";
+import { MetaPokemon } from "..";
+import { fakePokemon } from "../../../fixtures/data-sources/pokemon";
+
+const baseParent: MetaPokemon = {
+  id: "7",
+  _meta: {
+    pokemonDetails: fakePokemon,
+  },
+};
+
+describe("Pokemon.name", () => {
+  it("should return name as a string", () => {
+    const expectedResult = "squirtle";
+    const result = getName(baseParent);
+    expect(result).toBe(expectedResult);
+  });
+
+  it("should return null if _meta.pokemonDetails.name is undefined", () => {
+    const updatedParent = {
+      ...baseParent,
+      _meta: {
+        ...baseParent._meta,
+        pokemonDetails: {
+          ...fakePokemon,
+          name: undefined as unknown as string,
+        },
+      },
+    };
+    const result = getName(updatedParent);
+    expect(result).toBeNull();
+  });
+});
