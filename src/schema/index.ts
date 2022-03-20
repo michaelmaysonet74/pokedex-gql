542ce55c847d02be46bd27b6c2a98addf559c1ef
michaelmaysonet74 <michaelmaysonet74@gmail.com>
2022-03-19 21:06:13 -0400

    Separate schema into domain specific types

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -4,10 +4,22 @@ import { readFileSync } from "fs";
 import { resolve } from "path";
 import { resolvers } from "../resolvers";
 
-const typeDefs = gql(
-  readFileSync(resolve(__dirname, "schema.graphql"), {
-    encoding: "utf8",
-  })
-);
+const typeDefs = [
+  gql(
+    readFileSync(resolve(__dirname, "evolution.graphql"), {
+      encoding: "utf8",
+    })
+  ),
+  gql(
+    readFileSync(resolve(__dirname, "measurement.graphql"), {
+      encoding: "utf8",
+    })
+  ),
+  gql(
+    readFileSync(resolve(__dirname, "pokemon.graphql"), {
+      encoding: "utf8",
+    })
+  ),
+];
 
 export default makeExecutableSchema({ typeDefs, resolvers });
